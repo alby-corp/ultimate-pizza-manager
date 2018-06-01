6136131845c61d256be34ed7372519057091771a
roald <roalddierske@gmail.com>
2018-06-01 15:08:37 +0200

    v1.0.0

--- server/app.mjs
+++ server/app.mjs
@@ -26,12 +26,12 @@ export const App = (function () {
 
             app.use(express.urlencoded({extended: false}));
 
-            app.use('', express.static(path.join(__dirname, '../dist')));
+            app.use('', express.static(path.resolve(__dirname, '../dist')));
 
             app.use(bodyParser.json());
             app.use('/api', router.configuration);
 
-            app.use((req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')));
+            app.use((req, res) => res.sendFile(path.resolve(__dirname, '../dist/index.html')));
 
             privateProps.set(this, {
                 app: app
